9b0e4a847497d8af590bdc27695afaa9db63b1cd
For Fun <Fantomster@yandex.ru>
2018-05-08 17:34:18 +0300

    23.04.2018

--- ai/ensemble_learning/random_forests.py
+++ ai/ensemble_learning/random_forests.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report
 from sklearn import cross_validation
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
-from ai.utilities import visualize_classifier
+from utilities import visualize_classifier
 
 # Argument parser
 def build_arg_parser():
@@ -19,24 +19,24 @@ if __name__ == '__main__':
     classifier_type = args.classifier_type
 
 # Load input data
-input_file = 'data_random_forest.txt'
-data = np.loadtxt(input_file, delimiter=',')
-X, y = data[:, :-1], data[:, -1]
+# input_file = 'data_random_forest.txt'
+# data = np.loadtxt(input_file, delimiter=',')
+# X, y = data[:, :-1], data[:, -1]
 
 # Separate input data into three classes based on labels
-class_0 = np.array(X[y==0])
-class_1 = np.array(X[y==1])
-class_2 = np.array(X[y==2])
+# class_0 = np.array(X[y==0])
+# class_1 = np.array(X[y==1])
+# class_2 = np.array(X[y==2])
 
 # Visualize input data
-plt.figure()
-plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='s')
-plt.scatter(class_1[: 0], class_1[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='o')
-plt.scatter(class_2[:, 0], class_2[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='^')
-plt.title('Input data')
+# plt.figure()
+# plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='s')
+# plt.scatter(class_1[: 0], class_1[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='o')
+# plt.scatter(class_2[:, 0], class_2[:, 1], s=75, facecolors='white', edgecolors='black', linewidths=1, marker='^')
+# plt.title('Input data')
 
 # Split data into training and testing datasets
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=5)
+# X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=5)
 
 # Ensemble Learning classifier
 params = {'n_estimators': 100, 'max_depth': 4, 'random_state': 0}
@@ -46,22 +46,22 @@ if classifier_type == 'rf':
 else:
     classifier = ExtraTreesClassifier(**params)
 
-classifier.fit(X_train, y_train)
-visualize_classifier(classifier, X_train, y_train, 'Training dataset')
-
-y_test_pred = classifier.predict(X_test)
-visualize_classifier(classifier, X_test, y_test, 'Test dataset')
+# classifier.fit(X_train, y_train)
+# visualize_classifier(classifier, X_train, y_train, 'Training dataset')
+#
+# y_test_pred = classifier.predict(X_test)
+# visualize_classifier(classifier, X_test, y_test, 'Test dataset')
 
 # Evaluate classifier performance
-class_names = ['Class-0', 'Class-1', 'Class-2']
-print('\n' + '#'*40)
-print('\nClassifier performance on training dataset\n')
-print(classification_report(y_train, classifier.predict(X_train), target_names=class_names))
-print('#'*40 + '\n')
-print('#'*40)
-print('\nClassifier performance on test dataset\n')
-print(classification_report(y_test, y_test_pred, target_names=class_names))
-print('#'*40 + '\n')
+# class_names = ['Class-0', 'Class-1', 'Class-2']
+# print('\n' + '#'*40)
+# print('\nClassifier performance on training dataset\n')
+# print(classification_report(y_train, classifier.predict(X_train), target_names=class_names))
+# print('#'*40 + '\n')
+# print('#'*40)
+# print('\nClassifier performance on test dataset\n')
+# print(classification_report(y_test, y_test_pred, target_names=class_names))
+# print('#'*40 + '\n')
 
 # python3 random_forests.py --classifier-type rf
 # python3 random_forests.py --classifier-type erf
