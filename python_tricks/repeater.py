d5a96126975ee62b145c9c47d18fe25353122b92
fantomster <fantomster@yandex.ru>
2018-07-05 18:51:55 +0300

    5.06.2018

--- python_tricks/repeater.py
+++ python_tricks/repeater.py
@@ -12,22 +12,51 @@
 #     def __next__(self):
 #         return self.source.value
 
-class Repeater:
-    def __init__(self, value):
-        self.value = value
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.value
+# class Repeater:
+#     def __init__(self, value):
+#         self.value = value
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         return self.value
 
 # repeater = Repeater('hello')
 # for item in repeater:
 #     print(item)
 
-repeater = Repeater('Hello')
+# repeater = Repeater('Hello')
+# iterator = iter(repeater)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+class BoundedRepeater:
+    def __init__(self, value, max_repeats):
+        self.value = value
+        self.max_repeats = max_repeats
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.max_repeats:
+            raise StopIteration
+        self.count += 1
+        return self.value
+
+repeater = BoundedRepeater('Hello', 3)
+for item in repeater:
+    print(item)
+
+repeater = BoundedRepeater('Hello', 3)
 iterator = iter(repeater)
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+while True:
+    try:
+        item = next(iterator)
+    except StopIteration:
+        break
+    print(item)
+
