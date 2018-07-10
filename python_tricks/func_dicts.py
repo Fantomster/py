cf7548c0c42687c59ca25bd860dc7d3548c3a38f
fantomster <fantomster@yandex.ru>
2018-07-10 19:00:00 +0300

    14.06.2018

--- python_tricks/func_dicts.py
+++ python_tricks/func_dicts.py
@@ -7,3 +7,16 @@ cond = 'cond_a'
 func_dict[cond]()
 
 func_dict.get(cond, handle_default)()
+
+
+
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+print(dispatch_dict('mul', 2, 8))
+print(dispatch_dict('unknown', 2, 8))
