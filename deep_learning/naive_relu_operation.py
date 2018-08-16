c4d5c52523d86bb50eaa681148b1e57798d4ec59
k.silukov <k.silukov@extteam.ru>
2018-08-16 09:13:42 +0300

    16.08.2018

---
+++ deep_learning/naive_relu_operation.py
@@ -0,0 +1,33 @@
+def naive_relu(x):
+    assert len(x.shape) == 2 #x is a 2d numpy tensor
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i, j], 0)
+
+    return x
+
+def naive_add(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] += y[i, j]
+
+    return x
+
+def naive_add_matrix_and_vector(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[1] == y.shape[0]
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, y] += y[j]
+
+    return x
+
