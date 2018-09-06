301a7dff40ab5022c8d3de10242aec4c9178352b
Konstantin Silukov <silukov@mixcart.ru>
2018-09-06 10:08:20 +0300

    06.09.2018

--- deep_learning/reuters_multi_classifier.py
+++ deep_learning/reuters_multi_classifier.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from keras.datasets import reuters
 import numpy as np
 from deep_learning.common_functions.common import vectorize_sequences
@@ -35,3 +36,46 @@ model.add(layers.Dense(64, activation='relu', input_shape=(10000, )))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(46, activation='softmax'))
 
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+x_val = x_train[:4000]
+partial_x_train = x_train[4000:]
+
+y_val = one_hot_train_labels[:4000]
+partial_y_train = one_hot_train_labels[4000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
