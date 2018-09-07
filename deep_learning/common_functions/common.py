54f642a286a9b3bfad6329ecbe31245cc44299fa
Konstantin Silukov <silukov@mixcart.ru>
2018-09-07 17:48:38 +0300

    7.09.2018

--- deep_learning/common_functions/common.py
+++ deep_learning/common_functions/common.py
@@ -1,4 +1,5 @@
 import numpy as np
+from keras import models, layers
 
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
@@ -11,3 +12,23 @@ def to_one_hot(labels, dimension=46):
     for i, label in enumerate(labels):
         results[i, label] = 1.
     return results
+
+
+def build_model(train_data):
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+
+    return smoothed_points
